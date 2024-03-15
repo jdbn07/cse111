0e344400c4526e47a5abd92faa602af927defbf2
jdbn07 <jdbn_07@hotmail.com>
2024-03-14 23:53:06 -0500

    add

--- test_water_flow.py
+++ test_water_flow.py
@@ -1,4 +1,4 @@
-from water_flow import water_column_height, pressure_gain_from_water_eight
+from water_flow import water_column_height,pressure_gain_from_water_eight
 from pytest import approx
 import pytest
 
@@ -8,4 +8,4 @@ def test_water_column_height():
     assert water_column_height (0.0,10.0) == (7.5)
 
 def test_pressure_gain_from_water_height():
-    assert pressure_gain_from_water_eight(0.0) == approx (0.0)
+    assert pressure_gain_from_water_eight(0.0) == approx(0.0, abs=0.001)
