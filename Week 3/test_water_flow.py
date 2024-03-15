615634900b2068339b50e9abafb09b5bd3187f18
jdbn07 <jdbn_07@hotmail.com>
2024-03-15 00:17:46 -0500

    correction

--- Week 3/test_water_flow.py
+++ Week 3/test_water_flow.py
@@ -11,7 +11,7 @@ def test_water_column_height():
 
 def test_pressure_gain_from_water_height():
    assert pressure_gain_from_water_height(0.0) == approx(0.000, abs=0.001)
-   assert pressure_gain_from_water_height(30.0) == approx(295.628, abs=0.001)
+   assert pressure_gain_from_water_height(30.2) == approx(295.628, abs=0.001)
    assert pressure_gain_from_water_height(50.0) == approx(489.450, abs=0.001)
 
 def test_pressure_loss_from_pipe():
@@ -21,7 +21,7 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.048692,200.00,0.018,1.75)==approx(-113.008, abs=0.001)
     assert pressure_loss_from_pipe(0.048692,200.00,0.018,1.65)==approx(-100.462, abs=0.001)
     assert pressure_loss_from_pipe(0.286870,1000.00,0.013,1.65)==approx(-61.576, abs=0.001)
-    assert pressure_loss_from_pipe(0.286870,1800.75,0.013,1.55)==approx(-110.884, abs=0.001)
+    assert pressure_loss_from_pipe(0.286870,1800.75,0.013,1.65)==approx(-110.884, abs=0.001)
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
 pytest.main(["-v", "--tb=line", "-rN", __file__])
--- Week 3/water_flow.py
+++ Week 3/water_flow.py
@@ -7,5 +7,7 @@ def pressure_gain_from_water_height(height):
 
     pressure=(998.2*9.80665*height)/1000
     return pressure
+
 def pressure_loss_from_pipe(pipe_diameter,pipe_length, friction_factor, fluid_velocity):
     preloss=((-friction_factor)*(pipe_length)*(998.2)*(fluid_velocity**2))/(2000*pipe_diameter)
+    return preloss
